081edfab825e05c9d95fe0208c4b5b0807c18cb1
ML <labrecquemegan@hotmail.com>
2022-01-12 08:57:15 -0500

    added coloumns to html/linked JS. Still need to add more coloumns and rename them

---
+++ Develop/script.js
@@ -0,0 +1,6 @@
+// I want to have a daily date on the page
+// I want the colors to change with the time
+// I want to save changes to local storage
+// Alert Message: "I just saved to local storage"
+
+$("#currentDay").text(moment().format("MMM Do YY"));
